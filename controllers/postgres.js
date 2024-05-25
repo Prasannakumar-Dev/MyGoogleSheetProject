7414d66fe136f74df26115bac6ad927484040d3b
Prasanna <prasannakumar.akrabhi@gmail.com>
2024-05-25 15:58:01 +0530

    adding feature not to insert duplicate data

--- controllers/postgres.js
+++ controllers/postgres.js
@@ -28,14 +28,41 @@ async function createEmployeeTable() {
 
 async function insertEmployee(data) {
   const client = await pool.connect();
+  const [
+    first_name,
+    last_name,
+    position,
+    department,
+    email,
+    phone_number,
+  ] = data;
   try {
-    const insertQuery = `
-      INSERT INTO employees (first_name, last_name, position, department, email, phone_number)
-      VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING employee_id;
-    `;
-    const res = await client.query(insertQuery, data);
-    console.log("New employee ID:", res.rows[0].employee_id);
+    const duplicateExists = await client.query(
+      `SELECT employee_id FROM employees WHERE email = $1 OR phone_number = $2`,
+      [email, phone_number]
+    );
+
+    if (duplicateExists.rows.length > 0) {
+      // Check if any rows are returned
+      console.log(
+        `Employee with email ${email} or phone number ${phone_number} already exists. Skipping.`
+      );
+    } else {
+      const insertQuery = `
+        INSERT INTO employees (first_name, last_name, position, department, email, phone_number)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING employee_id;
+      `;
+      const res = await client.query(insertQuery, [
+        first_name,
+        last_name,
+        position,
+        department,
+        email,
+        phone_number,
+      ]);
+      console.log("New employee ID:", res.rows[0].employee_id);
+    }
   } catch (e) {
     console.error("Error inserting employee:", e);
   } finally {
